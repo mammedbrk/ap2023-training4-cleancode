87ba836e04af44135cace0e0dac06e4be6f39bcd
mammedbrk <mammedbrk@gmail.com>
2023-04-22 01:10:40 +0330

    Create Main

---
+++ src/main/java/com/mammedbrk/Main.java
@@ -0,0 +1,10 @@
+package com.mammedbrk;
+
+import com.mammedbrk.logic.Agent;
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        new Agent();
+    }
+}
